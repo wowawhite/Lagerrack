c96161b1838d0a7e3f46f0c7acabadde8f6db8fb
wowa <none@none.com>
2024-01-02 12:55:20 +0100

    added exception handling and notifi

--- LSTM_AE_model_create.py
+++ LSTM_AE_model_create.py
@@ -23,6 +23,7 @@ import keras as kr
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import EarlyStopping
 from LSTM_AE_custom_models import *
+from Notification_module import *
 
 warnings.filterwarnings('ignore')
 # program control flags
@@ -180,177 +181,187 @@ def create_sequences(x_sequence, y, time_steps=1):
     # TODO: apply fft for sequence here
     return np.array(xs), np.array(ys)
 
+try:
+    print("creating sub-sequences")
+    X_train, y_train = create_sequences(train[['close']], train['close'], model_parameters['time_steps'])
+    X_test, y_test = create_sequences(test[['close']], test['close'], model_parameters['time_steps'])
+    print("X_train.shape,y_train.shape: ", X_train.shape, y_train.shape)
+    print("X_test.shape,y_test.shape: ", X_test.shape, y_test.shape)
 
-print("creating sub-sequences")
-X_train, y_train = create_sequences(train[['close']], train['close'], model_parameters['time_steps'])
-X_test, y_test = create_sequences(test[['close']], test['close'], model_parameters['time_steps'])
-print("X_train.shape,y_train.shape: ", X_train.shape, y_train.shape)
-print("X_test.shape,y_test.shape: ", X_test.shape, y_test.shape)
+    # Task 5: Build an LSTM Autoencoder
 
-# Task 5: Build an LSTM Autoencoder
-timesteps = X_train.shape[1]
-num_features = X_train.shape[2]
 
-print("assembly model")
-X = Sequential()
-# TODO: select model here
-my_model = LSTM_AE_model_gamma1(X, X_train)
-my_model.compile(loss=model_parameters['my_loss'], optimizer=model_parameters['my_optimizer'])
-my_model.summary()
+    timesteps = X_train.shape[1]
+    num_features = X_train.shape[2]
 
-# layer_names=[layer.name for layer in my_model.layers]
-# print(layer_names)
+    print("assembly model")
+    X = Sequential()
+    # TODO: select model here
+    my_model = LSTM_AE_model_gamma1(X, X_train)
+    my_model.compile(loss=model_parameters['my_loss'], optimizer=model_parameters['my_optimizer'])
+    my_model.summary()
 
-tf.keras.utils.plot_model(my_model, show_shapes=True, to_file=out_dir + timestr + '_my_modelplot.png',
-                          show_layer_names=True)
+    # layer_names=[layer.name for layer in my_model.layers]
+    # print(layer_names)
 
-# Task 6: Train the Autoencoder
-# https://machinelearningmastery.com/how-to-stop-training-deep-neural-networks-at-the-right-time-using-early-stopping/
-early_stop = EarlyStopping(monitor=model_parameters['my_monitor'], patience=model_parameters['my_patience'],
-                           mode=model_parameters['my_mode'])  # if the monitored metric does not change -> exit
-my_history = my_model.fit(X_train, y_train, epochs=model_parameters['my_epochs'],
-                          batch_size=model_parameters['my_batch_size'],
-                          validation_split=model_parameters['my_validation_split'], callbacks=[early_stop],
-                          shuffle=False)
-# my_history = model.fit(X_train,y_train,epochs=10,batch_size=32,validation_split=0.1,shuffle=False)
+    tf.keras.utils.plot_model(my_model, show_shapes=True, to_file=out_dir + timestr + '_my_modelplot.png',
+                              show_layer_names=True)
 
-# save model while training with checkpoints
-# https://keras.io/api/callbacks/model_checkpoint/
-# https://www.tensorflow.org/tutorials/keras/save_and_load
+    # Task 6: Train the Autoencoder
+    # https://machinelearningmastery.com/how-to-stop-training-deep-neural-networks-at-the-right-time-using-early-stopping/
+    early_stop = EarlyStopping(monitor=model_parameters['my_monitor'], patience=model_parameters['my_patience'],
+                               mode=model_parameters['my_mode'])  # if the monitored metric does not change -> exit
+    my_history = my_model.fit(X_train, y_train, epochs=model_parameters['my_epochs'],
+                              batch_size=model_parameters['my_batch_size'],
+                              validation_split=model_parameters['my_validation_split'], callbacks=[early_stop],
+                              shuffle=False)
+    # my_history = model.fit(X_train,y_train,epochs=10,batch_size=32,validation_split=0.1,shuffle=False)
 
-tf.keras.models.save_model(my_model, out_dir + timestr + "_my_model.keras", overwrite=True)
+    # save model while training with checkpoints
+    # https://keras.io/api/callbacks/model_checkpoint/
+    # https://www.tensorflow.org/tutorials/keras/save_and_load
 
-with open(out_dir + timestr + "_my_trainhistory.pckl", 'wb') as file_pi:
-    pickle.dump(my_history, file_pi)
+    tf.keras.models.save_model(my_model, out_dir + timestr + "_my_model.keras", overwrite=True)
 
-# Task 7: Plot Metrics and Evaluate the Model
+    with open(out_dir + timestr + "_my_trainhistory.pckl", 'wb') as file_pi:
+        pickle.dump(my_history, file_pi)
 
-# Load our saved model
-model_loaded = tf.keras.models.load_model(out_dir + timestr + "_my_model.keras", compile=True)
-with open(out_dir + timestr + "_my_trainhistory.pckl", "rb") as file_pi:
-    my_history = pickle.load(file_pi)
+    # Task 7: Plot Metrics and Evaluate the Model
 
-print("predicting anomaly")
-X_train_pred = model_loaded.predict(X_train)
-train_mae_loss = pd.DataFrame(np.mean(np.abs(X_train_pred - X_train), axis=1), columns=['Error'])
-X_test_pred = model_loaded.predict(X_test)
-test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
+    # Load our saved model
+    model_loaded = tf.keras.models.load_model(out_dir + timestr + "_my_model.keras", compile=True)
+    with open(out_dir + timestr + "_my_trainhistory.pckl", "rb") as file_pi:
+        my_history = pickle.load(file_pi)
 
-# Task 8: Detect Anomalies in Data
-test_score_df = pd.DataFrame(test[model_parameters['time_steps']:])
-test_score_df['loss'] = test_mae_loss
-test_score_df['threshold'] = model_parameters['my_threshold']
-test_score_df['anomaly'] = test_score_df['loss'] > test_score_df[
-    'threshold']  # this yields T/F but could be adapted to anomaly score
-test_score_df['close'] = test[model_parameters['time_steps']:]['close']
-test_score_df.head()
-test_score_df.tail()
-
-# this prints test loss over time
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=test[model_parameters['time_steps']:]['date'], y=test_score_df['loss'], mode='lines',
-                         name='Test Loss'))
-fig.add_trace(go.Scatter(x=test[model_parameters['time_steps']:]['date'], y=test_score_df['threshold'], mode='lines',
-                         name='Threshold'))
-fig.update_layout(title='training loss to time - ' + timestr, xaxis_title='Time', yaxis_title='Loss', showlegend=True)
-fig.write_html(out_dir + timestr + "_my_loss_time.html")
-fig.show()
-
-# marking anomaly true/false depending on test score
-anomalies = test_score_df[test_score_df['anomaly'] == True]
-anomalies.head()
-
-myfresh_x = test[model_parameters['time_steps']:]['date']
-# myfresh_y = test[time_steps:]['close']
-myfresh_y = scaler.inverse_transform(test[model_parameters['time_steps']:]['close'].values.reshape(-1, 1))
-
-print("shapes testdata x,y,:", myfresh_x.shape, myfresh_y.shape)
-myotherfresh_x = anomalies['date']
-# myotherfresh_y = anomalies['close']
-myotherfresh_y = scaler.inverse_transform(anomalies['close'].values.reshape(-1, 1))
-print("shapes anomalies x,y,:", myotherfresh_x.shape, myotherfresh_y.shape)
-
-# this should plot anomaly datapoints over original data
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=myfresh_x, y=myfresh_y[:, 0], mode='lines', name='audio data points'))
-fig.add_trace(go.Scatter(x=myotherfresh_x, y=myotherfresh_y[:, 0], mode='markers', name='Anomaly'))
-fig.update_layout(title='Audio spectrum with anomalies - ' + timestr, xaxis_title='Time', yaxis_title='Audio spectrum',
-                  showlegend=True)
-fig.write_html(out_dir + timestr + "_my_anomalies.html")
-fig.show()
-
-# this prints test loss over epochs
-fig = go.Figure()
-loss_ticks = np.arange(1, len(my_history.history['loss']))
-val_loss_ticks = np.arange(1, len(my_history.history['val_loss']))
-# fig.add_trace(go.Scatter(x=model_parameters["my_epochs"],y=train_mae_loss['Error'],mode='lines',name='train loss',))
-# fig.add_trace(go.Scatter(x=model_parameters["my_epochs"],y=test_mae_loss['Error'],mode='lines',name='test loss'))
-fig.add_trace(go.Scatter(y=my_history.history['loss'], x=loss_ticks, mode='lines', name='train loss'))
-fig.add_trace(go.Scatter(y=my_history.history['val_loss'], x=val_loss_ticks, mode='lines', name='test loss'))
-fig.update_layout(title='training loss to epochs - ' + timestr, xaxis_title='epochs', yaxis_title='loss',
-                  showlegend=True)
-fig.write_html(out_dir + timestr + "_my_loss_epochs.html")
-fig.show()
-
-# saving hyperparameter
-runtime_stop = timeit.default_timer()
-runtime_time = runtime_stop - runtime_start
-model_parameters['my_trainingtime'] = runtime_time
-model_parameters['my_trainingsuccess'] = True
-save_training_parameters(model_parameters)
-print(f"Runtime: {runtime_time}")
-
-if USE_ANOTHERTESTFILE:
-    nok_sequence = read_flac_to_pandas("visc6_ultrasonic_nok", start_sec=model_parameters['my_nok_startsec'],
-                                       stop_sec=model_parameters['my_nok_stopsec'])
-    print("predicting anomaly in NOK sequence")
-
-    nok_sequence_size = int(len(nok_sequence) * model_parameters['train_test_split'])
-    # no test size. test_size = len(df) - train_size
-    nok_sequence = nok_sequence.iloc[0:nok_sequence_size]
-
-    nok_scaler = StandardScaler()
-    nok_scaler = nok_scaler.fit(nok_sequence[['close']])
-    nok_sequence['close'] = nok_scaler.transform(nok_sequence[['close']])
-
-    nok_X_train, nok_y_train = create_sequences(nok_sequence[['close']], nok_sequence['close'],
-                                                model_parameters['time_steps'])
-    nok_X_train_pred = model_loaded.predict(nok_X_train)
-    nok_mae_loss = pd.DataFrame(np.mean(np.abs(nok_X_train_pred - nok_X_train), axis=1), columns=['Error'])
+    print("predicting anomaly")
+    X_train_pred = model_loaded.predict(X_train)
+    train_mae_loss = pd.DataFrame(np.mean(np.abs(X_train_pred - X_train), axis=1), columns=['Error'])
+    X_test_pred = model_loaded.predict(X_test)
+    test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
 
     # Task 8: Detect Anomalies in Data
-    test_score_df = pd.DataFrame(nok_sequence[model_parameters['time_steps']:])
-    test_score_df['loss'] = nok_mae_loss
+    test_score_df = pd.DataFrame(test[model_parameters['time_steps']:])
+    test_score_df['loss'] = test_mae_loss
     test_score_df['threshold'] = model_parameters['my_threshold']
     test_score_df['anomaly'] = test_score_df['loss'] > test_score_df[
         'threshold']  # this yields T/F but could be adapted to anomaly score
-    test_score_df['close'] = nok_sequence[model_parameters['time_steps']:]['close']
+    test_score_df['close'] = test[model_parameters['time_steps']:]['close']
     test_score_df.head()
     test_score_df.tail()
-    nok_anomalies = test_score_df[test_score_df['anomaly'] == True]
-    nok_anomalies.head()
 
-    nok_myfresh_x = nok_sequence[model_parameters['time_steps']:]['date']
-    nok_myfresh_y = nok_scaler.inverse_transform(
-        nok_sequence[model_parameters['time_steps']:]['close'].values.reshape(-1, 1))
-
-    print("shapes testdata x,y,:", nok_myfresh_x.shape, nok_myfresh_y.shape)
-    nok_myotherfresh_x = nok_anomalies['date']
-    nok_myotherfresh_y = nok_scaler.inverse_transform(nok_anomalies['close'].values.reshape(-1, 1))
-    print("shapes anomalies x,y,:", nok_myotherfresh_x.shape, nok_myotherfresh_y.shape)
-
-    # plot original nok time series
+    # this prints test loss over time
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=nok_myfresh_x, y=nok_myfresh_y[:, 0], mode='lines', name='audio data points'))
-    fig.update_layout(title='Audio spectrum with NOK anomalies - ' + timestr, xaxis_title='Time',
-                      yaxis_title='Audio spectrum', showlegend=True)
-    fig.write_html(out_dir + timestr + "_my_nok_timeseries.html")
+    fig.add_trace(go.Scatter(x=test[model_parameters['time_steps']:]['date'], y=test_score_df['loss'], mode='lines',
+                             name='Test Loss'))
+    fig.add_trace(go.Scatter(x=test[model_parameters['time_steps']:]['date'], y=test_score_df['threshold'], mode='lines',
+                             name='Threshold'))
+    fig.update_layout(title='training loss to time - ' + timestr, xaxis_title='Time', yaxis_title='Loss', showlegend=True)
+    fig.write_html(out_dir + timestr + "_my_loss_time.html")
     fig.show()
+
+    # marking anomaly true/false depending on test score
+    anomalies = test_score_df[test_score_df['anomaly'] == True]
+    anomalies.head()
+
+    myfresh_x = test[model_parameters['time_steps']:]['date']
+    # myfresh_y = test[time_steps:]['close']
+    myfresh_y = scaler.inverse_transform(test[model_parameters['time_steps']:]['close'].values.reshape(-1, 1))
+
+    print("shapes testdata x,y,:", myfresh_x.shape, myfresh_y.shape)
+    myotherfresh_x = anomalies['date']
+    # myotherfresh_y = anomalies['close']
+    myotherfresh_y = scaler.inverse_transform(anomalies['close'].values.reshape(-1, 1))
+    print("shapes anomalies x,y,:", myotherfresh_x.shape, myotherfresh_y.shape)
+
     # this should plot anomaly datapoints over original data
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=nok_myfresh_x, y=nok_myfresh_y[:, 0], mode='lines', name='audio data points'))
-    fig.add_trace(go.Scatter(x=nok_myotherfresh_x, y=nok_myotherfresh_y[:, 0], mode='markers', name='Anomaly'))
-    fig.update_layout(title='Audio spectrum with NOK anomalies - ' + timestr, xaxis_title='Time',
-                      yaxis_title='Audio spectrum', showlegend=True)
-    fig.write_html(out_dir + timestr + "_my_nok_anomalies.html")
+    fig.add_trace(go.Scatter(x=myfresh_x, y=myfresh_y[:, 0], mode='lines', name='audio data points'))
+    fig.add_trace(go.Scatter(x=myotherfresh_x, y=myotherfresh_y[:, 0], mode='markers', name='Anomaly'))
+    fig.update_layout(title='Audio spectrum with anomalies - ' + timestr, xaxis_title='Time', yaxis_title='Audio spectrum',
+                      showlegend=True)
+    fig.write_html(out_dir + timestr + "_my_anomalies.html")
     fig.show()
+
+    # this prints test loss over epochs
+    fig = go.Figure()
+    loss_ticks = np.arange(1, len(my_history.history['loss']))
+    val_loss_ticks = np.arange(1, len(my_history.history['val_loss']))
+    # fig.add_trace(go.Scatter(x=model_parameters["my_epochs"],y=train_mae_loss['Error'],mode='lines',name='train loss',))
+    # fig.add_trace(go.Scatter(x=model_parameters["my_epochs"],y=test_mae_loss['Error'],mode='lines',name='test loss'))
+    fig.add_trace(go.Scatter(y=my_history.history['loss'], x=loss_ticks, mode='lines', name='train loss'))
+    fig.add_trace(go.Scatter(y=my_history.history['val_loss'], x=val_loss_ticks, mode='lines', name='test loss'))
+    fig.update_layout(title='training loss to epochs - ' + timestr, xaxis_title='epochs', yaxis_title='loss',
+                      showlegend=True)
+    fig.write_html(out_dir + timestr + "_my_loss_epochs.html")
+    fig.show()
+
+    # saving hyperparameter
+    runtime_stop = timeit.default_timer()
+    runtime_time = runtime_stop - runtime_start
+    model_parameters['my_trainingtime'] = runtime_time
+    model_parameters['my_trainingsuccess'] = True
+    save_training_parameters(model_parameters)
+    print(f"Runtime: {runtime_time}")
+
+    if USE_ANOTHERTESTFILE:
+        nok_sequence = read_flac_to_pandas("visc6_ultrasonic_nok", start_sec=model_parameters['my_nok_startsec'],
+                                           stop_sec=model_parameters['my_nok_stopsec'])
+        print("predicting anomaly in NOK sequence")
+
+        nok_sequence_size = int(len(nok_sequence) * model_parameters['train_test_split'])
+        # no test size. test_size = len(df) - train_size
+        nok_sequence = nok_sequence.iloc[0:nok_sequence_size]
+
+        nok_scaler = StandardScaler()
+        nok_scaler = nok_scaler.fit(nok_sequence[['close']])
+        nok_sequence['close'] = nok_scaler.transform(nok_sequence[['close']])
+
+        nok_X_train, nok_y_train = create_sequences(nok_sequence[['close']], nok_sequence['close'],
+                                                    model_parameters['time_steps'])
+        nok_X_train_pred = model_loaded.predict(nok_X_train)
+        nok_mae_loss = pd.DataFrame(np.mean(np.abs(nok_X_train_pred - nok_X_train), axis=1), columns=['Error'])
+
+        # Task 8: Detect Anomalies in Data
+        test_score_df = pd.DataFrame(nok_sequence[model_parameters['time_steps']:])
+        test_score_df['loss'] = nok_mae_loss
+        test_score_df['threshold'] = model_parameters['my_threshold']
+        test_score_df['anomaly'] = test_score_df['loss'] > test_score_df[
+            'threshold']  # this yields T/F but could be adapted to anomaly score
+        test_score_df['close'] = nok_sequence[model_parameters['time_steps']:]['close']
+        test_score_df.head()
+        test_score_df.tail()
+        nok_anomalies = test_score_df[test_score_df['anomaly'] == True]
+        nok_anomalies.head()
+
+        nok_myfresh_x = nok_sequence[model_parameters['time_steps']:]['date']
+        nok_myfresh_y = nok_scaler.inverse_transform(
+            nok_sequence[model_parameters['time_steps']:]['close'].values.reshape(-1, 1))
+
+        print("shapes testdata x,y,:", nok_myfresh_x.shape, nok_myfresh_y.shape)
+        nok_myotherfresh_x = nok_anomalies['date']
+        nok_myotherfresh_y = nok_scaler.inverse_transform(nok_anomalies['close'].values.reshape(-1, 1))
+        print("shapes anomalies x,y,:", nok_myotherfresh_x.shape, nok_myotherfresh_y.shape)
+
+        # plot original nok time series
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=nok_myfresh_x, y=nok_myfresh_y[:, 0], mode='lines', name='audio data points'))
+        fig.update_layout(title='Audio spectrum with NOK anomalies - ' + timestr, xaxis_title='Time',
+                          yaxis_title='Audio spectrum', showlegend=True)
+        fig.write_html(out_dir + timestr + "_my_nok_timeseries.html")
+        fig.show()
+        # this should plot anomaly datapoints over original data
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=nok_myfresh_x, y=nok_myfresh_y[:, 0], mode='lines', name='audio data points'))
+        fig.add_trace(go.Scatter(x=nok_myotherfresh_x, y=nok_myotherfresh_y[:, 0], mode='markers', name='Anomaly'))
+        fig.update_layout(title='Audio spectrum with NOK anomalies - ' + timestr, xaxis_title='Time',
+                          yaxis_title='Audio spectrum', showlegend=True)
+        fig.write_html(out_dir + timestr + "_my_nok_anomalies.html")
+        fig.show()
+
+except Exception as error:
+    print("sending failed mail with following error:")
+    print(error)
+    send_error_notification(error)
+else:
+    print("sending ok mail")
+    send_finish_notification()
---
+++ Notification_module.py
@@ -0,0 +1,25 @@
+# https://yagmail.readthedocs.io/en/latest/usage.html
+import yagmail
+from pathlib import Path
+import os
+import json
+# your machine will call you back if your super duper ML model is trained and skynet is ready to take over the world
+parent_dir = Path(__file__).resolve().parent
+nonpublic_dir =str(Path.joinpath(parent_dir, "nonpublic")) + os.sep
+my_creds_filename = "usercredentials.json"
+
+
+with open(nonpublic_dir+my_creds_filename) as json_file:
+    creds_data = json.load(json_file)
+
+
+def send_finish_notification():
+    key_filename = creds_data["my_credfile"]
+    yag = yagmail.SMTP(user=creds_data["my_email"], oauth2_file=nonpublic_dir+key_filename)
+    yag.send(to='trash_bin@gmx.de', contents="Job done.", subject = 'Python done fine')
+
+def send_error_notification(errorstring):
+    key_filename = creds_data["my_credfile"]
+    yag = yagmail.SMTP(user=creds_data["my_email"], oauth2_file=nonpublic_dir+key_filename)
+    yag.send(to='trash_bin@gmx.de', contents=str(errorstring), subject = "Job failed. Go fix it, human!")
+
